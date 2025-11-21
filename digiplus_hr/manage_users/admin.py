e7e5f994da0989b97e5046e0c01ea6e9a5e22b6c
josnel2 <josnelpamelfichieu@gmail.com>
2025-11-21 02:39:58 +0100

    completer le fichier admin

--- digiplus_hr/manage_users/admin.py
+++ digiplus_hr/manage_users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, OTP, Poste, Employe
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(OTP)
+admin.site.register(Poste)
+admin.site.register(Employe)
