e03603991ffcfd5c8e17407805d416f53ebb99eb
josnel2 <josnelpamelfichieu@gmail.com>
2026-02-25 15:13:09 +0100

    test

--- digiplus_hr/digiplus_hr/settings.py
+++ digiplus_hr/digiplus_hr/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.railway.app,localhost,127.0.0.1,digiplus-grh-production.up.railway.app').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='https://pamel.pythonanywhere.com/').split(',')
 
 
 # Application definition
