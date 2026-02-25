4b72818d266ea107a20238dc5e97cee8c1ae1fcf
josnel2 <josnelpamelfichieu@gmail.com>
2026-02-25 14:31:00 +0100

    swagger

--- digiplus_hr/digiplus_hr/urls.py
+++ digiplus_hr/digiplus_hr/urls.py
@@ -19,12 +19,18 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/api/users/', permanent=False)),
+    path('', RedirectView.as_view(url='/api/docs/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/users/', include('manage_users.urls')),
     path('api/management/', include('manage_users.urls')),
+
+    # Swagger / OpenAPI
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
